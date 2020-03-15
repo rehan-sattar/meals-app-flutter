2f83b0dbaa5a7026e8912553e6f17b9af845151d
rehan-sattar <rehansattar117@gmail.com>
2020-03-16 02:05:12 +0500

    ✨ - add filters to filters screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,16 +2,98 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({Key key}) : super(key: key);
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _isGlutenFree = false;
+  var _isVegan = false;
+  var _isVegetarian = false;
+  var _isLactoseFree = false;
+
+  Widget buildSwithTilesForFilters(
+    String titleText,
+    String subTitleText,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(titleText),
+      subtitle: Text(subTitleText),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Filters'),
+        title: Text('Your Filters '),
       ),
       drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                buildSwithTilesForFilters(
+                  'Gluten Free',
+                  'Only includes gluten free food.',
+                  _isGlutenFree,
+                  (newValue) {
+                    setState(() {
+                      _isGlutenFree = newValue;
+                    });
+                  },
+                ),
+                buildSwithTilesForFilters(
+                  'Vegitarian',
+                  'Only includes Vegitarian food.',
+                  _isVegetarian,
+                  (newValue) {
+                    setState(() {
+                      _isVegetarian = newValue;
+                    });
+                  },
+                ),
+                buildSwithTilesForFilters(
+                  'Vegan',
+                  'Only includes vegan food.',
+                  _isVegan,
+                  (newValue) {
+                    setState(() {
+                      _isVegan = newValue;
+                    });
+                  },
+                ),
+                buildSwithTilesForFilters(
+                  'Lactose Free',
+                  'Only includes lactose free food.',
+                  _isLactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _isLactoseFree = newValue;
+                    });
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
